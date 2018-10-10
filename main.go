f88e37519cf8f9658b97ebdcfb6932b34c37aee6
Marcus Mathiassen <mathiassenmarcus@me.com>
2018-10-10 18:27:49 +0200

    cont

--- main.go
+++ main.go
@@ -2,15 +2,36 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+var startTime = time.Now()
+
+func fmtDuration(duration time.Duration) string {
+	days := int64(duration.Hours() / 24)
+	years := days / 365
+	months := years / 12
+	hours := int64(math.Mod(duration.Hours(), 24))
+	minutes := int64(math.Mod(duration.Minutes(), 60))
+	seconds := int64(math.Mod(duration.Seconds(), 60))
+
+	return fmt.Sprintf("P%dY%dM%dDT%dH%dM%dS", years, months, days, hours, minutes, seconds)
+}
+
+func getUptime() string {
+	return fmtDuration(time.Since(startTime))
+}
 
 func handleBadRequest(c *gin.Context) {
 	fmt.Println("handleBadRequest ...")
 }
 
 func handleRequest(c *gin.Context) {
+	c.String(http.StatusOK, "Hello %s", c.Param("name"))
 	fmt.Println("handleRequest ...")
 }
 
@@ -24,7 +45,14 @@ func main() {
 	// fmt.Printf("Pilot: %s, gliderType: %s, date: %s\n", track.Pilot, track.GliderType, track.Date.String())
 
 	r := gin.Default()
-	r.GET("/", handleBadRequest)
-	r.GET("/igcinfo/api", handleRequest)
+	r.GET("/igcinfo/api", func(c *gin.Context) {
+		c.Writer.Header().Set("Content-Type", "application/json")
+		c.IndentedJSON(http.StatusOK, gin.H{
+			"uptime":  getUptime(),
+			"info":    "Service for IGC tracks.",
+			"version": "v1",
+		})
+	})
+	r.GET("/igcinfo/api/:name", handleRequest)
 	r.Run()
 }
